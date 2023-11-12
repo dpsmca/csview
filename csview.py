7a5bf5a067b64b2dde4645e99db68d5e38ce7039
David Sargeant <sargeant.david@mayo.edu>
2023-11-12 03:08:40 -0600

    Add comment about available colors from termcolor

--- csview.py
+++ csview.py
@@ -26,6 +26,18 @@ PADDING_LEFT = 0
 PADDING_RIGHT = 2
 
 # Define a list of colors to be used for the columns.
+# Available text colors:
+#     black, red, green, yellow, blue, magenta, cyan, white,
+#     light_grey, dark_grey, light_red, light_green, light_yellow, light_blue,
+#     light_magenta, light_cyan.
+#
+# Available text highlights:
+#     on_black, on_red, on_green, on_yellow, on_blue, on_magenta, on_cyan, on_white,
+#     on_light_grey, on_dark_grey, on_light_red, on_light_green, on_light_yellow,
+#     on_light_blue, on_light_magenta, on_light_cyan.
+#
+# Available attributes:
+#     bold, dark, underline, blink, reverse, concealed.
 # colors = ['red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white', 'grey', 'light_red', 'light_green']
 colors = ['red', 'light_green', 'yellow', 'blue', 'magenta', 'cyan', 'light_red', 'yellow', 'light_cyan', 'white']
 color_comment = "grey"
