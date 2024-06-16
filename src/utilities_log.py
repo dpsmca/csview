4f93ecbdb0edf779dd1eff99478fa924b0c597ae
David Sargeant <sargeant.david@mayo.edu>
2024-06-15 19:13:31 -0500

    Update log utilities

--- src/utilities_log.py
+++ src/utilities_log.py
@@ -6,10 +6,13 @@ import pytz
 
 from termcolor import colored, cprint
 
-from config import PipelineConfig, CONFIG, OPTIONS
-TZ = CONFIG['TIMEZONE'] if 'TIMEZONE' in CONFIG else CONFIG['DEFAULT_TIMEZONE']
+TZ = "US/Central"
 TIMEZONE = pytz.timezone(TZ)
+
 DEFAULT_DEBUG = False
+
+OPTIONS = dict()
+OPTIONS['DEBUG'] = DEFAULT_DEBUG
 
 
 def timestamp() -> str:
@@ -26,20 +29,20 @@ def logDbg(*args, **kwargs):
     output_debug = OPTIONS['DEBUG'] if OPTIONS is not None and 'DEBUG' in OPTIONS else DEFAULT_DEBUG
     if output_debug:
         ts = timestamp()
-        print(colored(f"[ {ts} ] [ DEBUG ]", 'magenta'), " ".join(map(str, args)), **kwargs, file=sys.stderr)
+        print(colored(f"[{ts}] [DEBUG]", 'magenta'), " ".join(map(str, args)), **kwargs, file=sys.stderr)
 
 
 def logErr(*args, **kwargs):
     ts = timestamp()
-    print(colored(f"[ {ts} ] [ ERROR ] ", 'red'), " ".join(map(str, args)), **kwargs, file=sys.stderr)
+    print(colored(f"[{ts}] [ERROR] ", 'red'), " ".join(map(str, args)), **kwargs, file=sys.stderr)
 
 
 def logWarn(*args, **kwargs):
     ts = timestamp()
-    print(colored(f"[ {ts} ] [ WARNING ] ", 'yellow'), " ".join(map(str, args)), **kwargs, file=sys.stderr)
+    print(colored(f"[{ts}] [WARNING] ", 'yellow'), " ".join(map(str, args)), **kwargs, file=sys.stderr)
 
 
 def logDryRun(*args, **kwargs):
-    print(colored(f"[ DRY RUN ]", 'magenta'), " ".join(map(str, args)), **kwargs, file=sys.stderr)
+    print(colored(f"[DRY RUN]", 'magenta'), " ".join(map(str, args)), **kwargs, file=sys.stderr)
 
 
